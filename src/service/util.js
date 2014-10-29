3249890d8987a2d4d0e81d1d11883e00a54a4c84
Jianbing Fang <jianbingfang@gmail.com>
2014-10-29 17:52:39 +0800

    Update util.js

--- src/service/util.js
+++ src/service/util.js
@@ -2,14 +2,31 @@
 
 var school = require('../model/keySchool');
 
+var isInSchoolList = function (list, name) {
+
+    for (var school in list) {
+        var tag = true;
+        for (var i = 0; i < school.length; i++) {
+            if (name.indexOf(school.charAt(i)) === -1) {
+                tag = false;
+                break;
+            }
+        }
+        if (tag === true) {
+            return true;
+        }
+    }
+
+    return false;
+};
+
 var isKeyJuniorSchool = function (name) {
-    return school.getKeyJuniorSchool.indexOf(name) !== -1;
+    return isInSchoolList(school.getKeyJuniorSchool(), name);
 };
 
 var isKeySeniorSchool = function (name) {
-    return school.getKeyJuniorSchool.indexOf(name) !== -1;
+    return isInSchoolList(school.getKeySeniorSchool(), name);
 };
-
 
 module.exports.isKeyJuniorSchool = isKeyJuniorSchool;
 module.exports.isKeySeniorSchool = isKeySeniorSchool;
