61a082a4349bb6b66caf3f4267052903e8f583b0
Jianbing Fang <jianbingfang@gmail.com>
2014-11-13 18:07:45 +0800

    add for-in filter

--- src/service/util.js
+++ src/service/util.js
@@ -50,8 +50,7 @@ var distanceBetweenPoints = function (p1, p2) {
     var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
         Math.cos(p1.lat * Math.PI / 180) * Math.cos(p2.lat * Math.PI / 180) * Math.sin(dLon / 2) * Math.sin(dLon / 2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
-    return d;
+    return R * c;
 };
 
 var normalizeScore = function (param) {
